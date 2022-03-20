37bd1bab8a794e98490c00282739ce4029aa52a3
lakshya701 <91790396+lakshya701@users.noreply.github.com>
2022-03-20 20:45:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+
+var database;
+var back_img;
+var climb_coconut
+var man
+var man_shake
+var coconut_tree
+var aboriginal
+var man_inforest 
+
+function preload(){
+ back_img = loadImage(forest.jpg)
+ img = loadImage(climb.jpg)
+ img = loadImage (shake.jpg)
+ img = loadImage (man.jpg)
+ img = loadImage(oboriginal.jpg)
+
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+
+ 
+}
+
+function draw(){
+  background("white");
+ 
+  
+drawSprites()
+}
